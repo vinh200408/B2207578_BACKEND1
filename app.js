7a2aba921fc23a8f93c5dda3e7f7afd9abf95d09
vinh200408 <vinhb2207578@student.ctu.edu.vn>
2025-07-24 03:36:50 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -12,5 +13,15 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
