cd62e276026f3cacc775dc00f8616518bde7559f
vinh200408 <vinhb2207578@student.ctu.edu.vn>
2025-07-24 02:30:02 +0700

    Cai dat express chay thong bao xin chao

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,30 @@
+// eslint.config.mjs
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,cjs}"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: { ...globals.node },
+    },
+    plugins: {
+      js,
+    },
+    extends: ["plugin:js/recommended"],
+  },
   tseslint.configs.recommended,
   pluginVue.configs["flat/essential"],
-  { files: ["**/*.vue"], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+  {
+    files: ["**/*.vue"],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
 ]);
